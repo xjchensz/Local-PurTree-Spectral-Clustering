3f0d020bd31f2fe1e910ddf5fe7595f51afd855b
haoqipaopao <1021908134@qq.com>
2016-01-15 14:51:24 +0800

    LPT
    LPT

---
+++ normal_LPT.m
@@ -0,0 +1,16 @@
+%%
+function [ W ] = normal_LPT( W )
+%NORMAL_LPT Summary of this function goes here
+%   Detailed explanation goes here
+
+y=sum(W,1); 
+for i=1:size(W,1);
+  if y(i,1)==0
+    W(i,:)=1/size(W,2);
+  else
+    W(i,:)=W(i,:)/(y(i:1)+eps);
+  end
+end
+
+end
+%%
