b79e95306f8c9ebdd74f4fa2f921428aaa01d5e5
Admin <Admin@Lenovo-PC>
2016-01-15 23:05:29 +0800

    correct positiveNorm

--- positiveNorm.m
+++ positiveNorm.m
@@ -9,12 +9,12 @@ function [x iter] = positiveNorm(v)
 iter=1;
  
 %vmax = max(v0);
-vmin = min(v0);
+vmin = min(v);
 if vmin < 0
     f = 1;
     lambda_m = 0;
     while abs(f) > 10^-10
-        v1 = v0 - lambda_m;
+        v1 = v - lambda_m;
         posidx = v1>0;
         npos = sum(posidx);
         g = -npos;
@@ -29,5 +29,5 @@ if vmin < 0
     x = max(v1,0);
 
 else
-    x = v0;
+    x = v;
 end;
