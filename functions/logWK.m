0934bbafc15929b4f265e910717a3fb3be49952d
xjc <xjc@DESKTOP-9NIBCCR>
2016-01-17 11:52:37 +0800

    add logwk

---
+++ functions/logWK.m
@@ -0,0 +1,12 @@
+function [ lw ] = logWK( distX, y )
+%LOGWK Summary of this function goes here
+%   Detailed explanation goes here
+cluster=unique(y);
+lw=0;
+for i=1:length(cluster)
+    idx=find(y==cluster(i));
+    lw=lw+sum(sum(distX(idx,idx)))/length(idx);
+end
+lw=log(lw/2);
+end
+
