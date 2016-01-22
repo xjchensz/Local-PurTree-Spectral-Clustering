655f541fccb21315fbf12f2305e2a065adceecf8
haoqipaopao <1021908134@qq.com>
2016-01-22 17:21:55 +0800

    add Q

---
+++ computeQ.m
@@ -0,0 +1,15 @@
+function [ Q ] = Q( distX, y )
+%LOGWK Summary of this function goes here
+%   Detailed explanation goes here
+cluster=unique(y);
+Q=0;
+
+m=sum(sum(distX));
+d=sum(distX,2);
+for i=1:length(cluster)
+    idx=find(y==cluster(i));
+    Q=Q+sum(sum(distX(idx,idx)- (d(idx)*d(idx)')/(2*m)));
+end
+Q=Q/(2*m);
+
+
